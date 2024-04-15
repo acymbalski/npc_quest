acf95a9978fb8ee1d50a62f32bc861976f7da922
acymbalski <acymbalski@gmail.com>
2024-04-15 15:11:00 -0400

    Display file saves to user

--- title.py
+++ title.py
@@ -1,7 +1,18 @@
 import pygame
 from enums import GameState
 from display import printMe
+from character import loadGame
 
+NUM_SAVES = 10
+
+
+def findSaveGames():
+    characters = []
+
+    for i in range(NUM_SAVES):
+        characters.append(loadGame(i))
+
+    return characters
 
 
 # Load the background image
@@ -22,5 +33,21 @@ def title_screen(screen):
     printMe(screen, "Credits: Everything by Mike Hommel", 20, 460)
     printMe(screen, "Copyright 2003, by Hamumu Software", 20, 500)
 
+    characters = findSaveGames()
+
+    for i in range(NUM_SAVES):
+        # if cursor == i:
+        # pygame.draw.rect(screen, (255, 255, 0), (38, 188 + i * 10, 310, 188 + 10 + i * 10))
+        character = characters[i]
+        if character:
+            s = f"{i+1: >2}. {character.name}, Lvl {character.level}, Score: {character.score}"
+
+        else:
+            s = f"{i+1: >2}. Unused"
+
+        # draw bounding box if mouseover
+        printMe(
+            screen, s, 40, 190 + i * 10, draw_bounding_box=True, bounding_box_width=268
+        )
 
     return GameState.TITLE
