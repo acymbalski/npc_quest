7f8cb29f939c8293751322899b63eb877b74686a
acymbalski <acymbalski@gmail.com>
2024-04-15 15:47:15 -0400

    Refactor game logic

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 import pygame
-from enum import Enum
 import title
 from enums import GameState
 
 
-def main():
-    # Initialize Pygame
-    pygame.init()
+class Game:
 
-    # Set up the window dimensions
-    width = 800
-    height = 600
-    screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption("NPC Quest R")
-    # Set the window icon
-    icon_image = pygame.image.load("graphics/fatbird.tga")
-    pygame.display.set_icon(icon_image)
+    def __init__(self):
+        # Initialize Pygame
+        pygame.init()
 
-    # Set the user's mouse cursor
-    cursor_image = pygame.image.load(
-        "graphics/cursor.tga"
-    ).convert_alpha()  # Convert the cursor image to a surface
+        # Set up the window dimensions
+        width = 800
+        height = 600
+        self.screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("NPC Quest R")
+        # Set the window icon
+        icon_image = pygame.image.load("graphics/fatbird.tga")
+        pygame.display.set_icon(icon_image)
 
-    def set_custom_cursor(cursor_surface, hotspot_x, hotspot_y):
+        self.title = title.Title(self)
+
+        # Set the user's mouse cursor
+        self.cursor_image = pygame.image.load(
+            "graphics/cursor.tga"
+        ).convert_alpha()  # Convert the cursor image to a surface
+
+        self.set_custom_cursor(self.cursor_image, hotspot_x=0, hotspot_y=0)
+
+        self.game_state = GameState.TITLE
+
+    def set_custom_cursor(self, cursor_surface, hotspot_x, hotspot_y):
         pygame.mouse.set_visible(False)
         cursor_size = cursor_surface.get_size()
         cursor_surface.set_colorkey((255, 0, 255))  # key out pink for transparancy
@@ -35,42 +42,46 @@ def main():
         )
         pygame.mouse.set_cursor(*cursor_tuple)
 
-    set_custom_cursor(cursor_image, hotspot_x=0, hotspot_y=0)
+    def run(self):
 
-    # Main game loop
-    running = True
-    game_state = GameState.TITLE
+        # Main game loop
+        running = True
 
-    while running:
-        # fill black
-        screen.fill((0, 0, 0))
+        while running:
+            # fill black
+            self.screen.fill((0, 0, 0))
 
-        if game_state == GameState.QUIT:
-            running = False
-
-        if game_state == GameState.TITLE:
-            game_state = title.title_screen(screen)
-
-        elif game_state == GameState.SHOP:
-            pass
-
-        elif game_state == GameState.GAME:
-            pass
-
-        # Event handling
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if self.game_state == GameState.QUIT:
                 running = False
 
-        # Draw the cursor
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-        screen.blit(cursor_image, (mouse_x, mouse_y))
+            if self.game_state == GameState.TITLE:
+                self.title.update()
 
-        # Update the display
-        pygame.display.flip()
+            elif self.game_state == GameState.SHOP:
+                pass
 
-    # Quit Pygame
-    pygame.quit()
+            elif self.game_state == GameState.GAME:
+                pass
+
+            # Event handling
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            # Draw the cursor
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            self.screen.blit(self.cursor_image, (mouse_x, mouse_y))
+
+            # Update the display
+            pygame.display.flip()
+
+        # Quit Pygame
+        pygame.quit()
+
+
+def main():
+    game = Game()
+    game.run()
 
 
 if __name__ == "__main__":
--- title.py
+++ title.py
@@ -1,5 +1,4 @@
 import pygame
-from enums import GameState
 from display import printMe
 from character import loadGame
 
@@ -19,46 +18,57 @@ def findSaveGames():
 background_image = pygame.image.load("graphics/title.tga")
 
 
-def title_screen(screen):
-    # width, height = screen.get_size()
-    # Draw the background image
-    screen.blit(
-        background_image, (10, 40)
-    )  # currently matching the original game's offset
+class Title:
 
-    # draw welcome text
-    printMe(screen, "Select a game slot to play from!", 40, 160)
-    printMe(screen, "(Right-click a slot to erase it)", 40, 170)
-    printMe(screen, "Press ESC to stop being entertained", 40, 320)
-    printMe(
-        screen,
-        "Exit To Windows!",
-        40,
-        290,
-        draw_bounding_box=True,
-        bounding_box_width=268,
-    )
-    printMe(screen, "Credits: Everything by Mike Hommel", 20, 460)
-    printMe(screen, "Copyright 2003, by Hamumu Software", 20, 500)
+    def __init__(self, game):
+        self.game = game
 
-    characters = findSaveGames()
+    def update(self):
 
-    for i in range(NUM_SAVES):
-        # if cursor == i:
-        # pygame.draw.rect(screen, (255, 255, 0), (38, 188 + i * 10, 310, 188 + 10 + i * 10))
-        character = characters[i]
-        if character:
-            s = f"{i+1: >2}. {character.name}, Lvl {character.level}, Score: {character.score}"
+        screen = self.game.screen
 
-        else:
-            s = f"{i+1: >2}. Unused"
+        # width, height = screen.get_size()
+        # Draw the background image
+        screen.blit(
+            background_image, (10, 40)
+        )  # currently matching the original game's offset
 
-        # draw bounding box if mouseover
+        # draw welcome text
+        printMe(screen, "Select a game slot to play from!", 40, 160)
+        printMe(screen, "(Right-click a slot to erase it)", 40, 170)
+        printMe(screen, "Press ESC to stop being entertained", 40, 320)
         printMe(
-            screen, s, 40, 190 + i * 10, draw_bounding_box=True, bounding_box_width=268
+            screen,
+            "Exit To Windows!",
+            40,
+            290,
+            draw_bounding_box=True,
+            bounding_box_width=268,
         )
+        printMe(screen, "Credits: Everything by Mike Hommel", 20, 460)
+        printMe(screen, "Copyright 2003, by Hamumu Software", 20, 500)
 
-    return GameState.TITLE
+        characters = findSaveGames()
+
+        for i in range(NUM_SAVES):
+            # if cursor == i:
+            # pygame.draw.rect(screen, (255, 255, 0), (38, 188 + i * 10, 310, 188 + 10 + i * 10))
+            character = characters[i]
+            if character:
+                s = f"{i+1: >2}. {character.name}, Lvl {character.level}, Score: {character.score}"
+
+            else:
+                s = f"{i+1: >2}. Unused"
+
+            # draw bounding box if mouseover
+            printMe(
+                screen,
+                s,
+                40,
+                190 + i * 10,
+                draw_bounding_box=True,
+                bounding_box_width=268,
+            )
 
 
 if __name__ == "__main__":
