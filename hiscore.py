0cc80f78604265745fb30c3e80ffdf4ba661f3b0
acymbalski <acymbalski@gmail.com>
2024-04-18 12:03:48 -0400

    Hiscore work

--- hiscore.py
+++ hiscore.py
@@ -2,11 +2,35 @@ from character import className
 from constants import LEVELS, MAX_SCORES
 from display import printMe
 
+# TODO: make sure we save this to hiscore.dat somewhere
+
+
+class Score:
+    def __init__(self, name, chrClass, deathCause, deathLevel, maxLevel, score):
+        self.name = name
+        self.chrClass = chrClass
+        self.deathCause = deathCause
+        self.deathLevel = deathLevel
+        self.maxLevel = maxLevel
+        self.score = score
+
+    def __str__(self):
+        # TODO: Note this doesn't include the prefix of the actual number of the rank, include that elsewhere
+        return f"{self.name}, a Level {self.maxLevel} {className(self.chrClass)}. Score: {self.score}"
+
 
 def rankEarned(game):
-    # TODO
+    # TODO: add hiscore list to game, sort it here by score, return rank
+    rank = 0
     for i in range(MAX_SCORES):
-        pass
+        if game.player.xp > game.hiscore[i].score:
+            pass
+    return rank
+
+
+def drawHiScores(game):
+    printMe(game, "Greatest NPCs In History", 360, 5)
+    # TODO: the rest
 
 
 def drawDeathScore(game):
